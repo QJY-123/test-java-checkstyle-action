2a999df4d65f217e475f4fa9e836db0431da870c
QJY-123 <1685983946@qq.com>
2021-07-03 09:32:54 +0800

    代码修改之后再提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+/**
+ * 主类.
+ */
+public final class Main {
+    /**
+     * 构造函数.
+     */
+    private Main() {
     }
+  /**
+   * 主函数.
+   *
+   * @param args 不知道是啥.
+   */
+  public static void main(final String[] args) {
+    // write your code here
+  }
 }
